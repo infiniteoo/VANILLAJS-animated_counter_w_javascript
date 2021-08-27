d6042c75eac42f98c32ef1ad26c3fe31597df4a8
Troy Dorman <troydorman@gmail.com>
2021-08-26 19:32:33 -0600

    got rid of decimals in the counting

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ counters.forEach((counter) => {
     const inc = target / speed;
 
     if (count < target) {
-      counter.innerText = count + inc;
+      counter.innerText = Math.ceil(count + inc);
       setTimeout(updateCount, 1);
     } else {
       count.innerText = target;
